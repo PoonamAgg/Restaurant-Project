9ea3c31d40974507fca60505d70cdd385905972f
PoonamAgg <agarwalit09>
2023-12-26 17:13:47 +0530

    Cart Without any functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Fragment } from 'react';
 import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals'
+import Cart from './components/Cart/Cart';
+import Card from './components/UI/Card';
 
 function App() {
   return (
     <Fragment>
+      <Cart/>
       <Header/>
       <main>
         <Meals/>
